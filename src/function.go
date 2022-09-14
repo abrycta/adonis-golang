7985b0de4415ebd0c00da9a49f84a0280a958a5c
clarenz23 <112999859+clarenz23@users.noreply.github.com>
2022-09-14 14:44:03 +0800

    Functions and Sorting by Functions

---
+++ src/function.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+//function that takes two ints and return their product
+func product(a int, b int) int {
+    return a * b
+}
+
+//function that takes two ints and return their sum
+func sum(a int, b int) int {
+    return a + b
+}
+
+//function that takes three ints and return their sum
+func sumOfThree(a int, b int, c int) int {
+    return a + b + c
+}
+
+func main() {
+
+    //call a function using name(args)
+    resultSum := sum(5, 10)
+    fmt.Println("5+10 =", resultSum)
+
+    resultProduct := product(4, 5)
+    fmt.Println("4*5 =", resultProduct)
+
+    resultSumOfThree := sumOfThree(5, 10, 15)
+    fmt.Println("5+10+15 =", resultSumOfThree)
+
+}
