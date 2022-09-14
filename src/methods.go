9954e0db5fcb0a1a41e09f3b1a0edf6c0b396f6d
clarenz23 <112999859+clarenz23@users.noreply.github.com>
2022-09-14 21:51:48 +0800

    Update methods.go

--- src/methods.go
+++ src/methods.go
@@ -2,6 +2,7 @@ package main
 
 import "fmt"
 
+//method that has a receiver type of *rect
 type rect struct {
     width, height int
 }
@@ -17,9 +18,11 @@ func (r rect) perim() int {
 func main() {
     r := rect{width: 10, height: 5}
 
+    //call 2 methods defined for the struct
     fmt.Println("area: ", r.area())
     fmt.Println("perim:", r.perim())
 
+    //use pointer receiver to avoid copying on method calls
     rp := &r
     fmt.Println("area: ", rp.area())
     fmt.Println("perim:", rp.perim())
