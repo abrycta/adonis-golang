007cee8d33204ea3cdb5270b8a447938ffa526dc
Kurt <2214958@slu.edu.ph>
2022-09-16 08:30:35 +0800

    Modify comments of pointers in pointer.go

--- src/07pointer.go
+++ src/07pointer.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
-// function that increments a variable that its argument points to and returns the pointer
-// of the manipulated variable
+// function that increments a variable that its argument points to
+// and returns the pointer of the modified variable
+// the * operator has 2 forms in the context of pointers:
+//  1. in front of a base type, e.g.: *int
+//  2. in front of a variable name, e.g.: *x
+//
+// For case 1, it means that something is expecting or returning
+// a pointer to an int. Case 1 is a TYPE
+// For case 2, you are accessing the variable of the pointer.
+// Case 2 is an OPERATION
 func incr(a *int) int {
-	*a++
+	*a++ // DEREFERENCE a and increment the value stored
+	// at that address in memory
 	return *a
 }
 
