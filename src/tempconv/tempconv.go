f8d4e1c4656d0a8606d879fd408ce6c5074b7551
Marko <2212093@slu.edu.ph>
2022-09-16 19:17:16 +0800

    added comments tempconv.go, tempconvTester.go

--- src/tempconv/tempconv.go
+++ src/tempconv/tempconv.go
@@ -1,22 +1,22 @@
 // Package tempconv performs Celsius and Fahrenheit conversions.
-package tempconv
+package tempconv // creation and naming of a package
 
 import "fmt"
 
-type Celsius float64
+type Celsius float64 //variable declarations
 type Fahrenheit float64
 
 const (
-	AbsoluteZeroC Celsius = -273.15
+	AbsoluteZeroC Celsius = -273.15 // Initialization of constant variables
 	FreezingC     Celsius = 0
 	BoilingC      Celsius = 100
 )
 
-func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) } // Functions that return the computed values to string
 func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
 
 // CToF converts a Celsius temperature to Fahrenheit.
-func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) } // Functions that compute the conversion of the following
 
 // FToC converts a Fahrenheit temperature to Celsius.
 func FToC(f Celsius) Celsius { return Celsius((f - 32) * 5 / 9) }
--- src/tempconvTester.go
+++ src/tempconvTester.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"src/tempconv"
+	"src/tempconv" // Importing path of the package created
 )
 
-func main() {
-	fmt.Printf("Absolute Zero = %v\n", tempconv.AbsoluteZeroC) // "(Conversion) Absolute Zero= -273.15°C"
-	fmt.Println(tempconv.CToF(tempconv.BoilingC))              // (Conversion) "212°F"
-	fmt.Println(tempconv.FToC(tempconv.FreezingC))             // (Conversion) "-17.77777777777778°C"
+func main() { // Conversion tester
+	fmt.Printf("Absolute Zero = %v\n", tempconv.AbsoluteZeroC) // Print the computed values Absolute Zero= -273.15°C"
+	fmt.Println(tempconv.CToF(tempconv.BoilingC))              // "212°F"
+	fmt.Println(tempconv.FToC(tempconv.FreezingC))             // "-17.77777777777778°C"
 }
