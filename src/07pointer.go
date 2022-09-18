994b3a8d3362c3659ee1e26dbe425a5e3b828dcd
jerome <2213540@slu.edu.ph>
2022-09-18 23:12:36 +0800

    minor changes

--- src/07pointer.go
+++ src/07pointer.go
@@ -5,10 +5,9 @@ import "fmt"
 func main() {
 
 	x := 1
-	var p *int      //*int = base type
-	p = &x          //p, of type *int, points to x
-	fmt.Println(*p) //prints "1"
-	*p = 2          //assign/updates the value of x
-	fmt.Println(x)  //prints the new value that is "2"
-
+	var p *int               //*int = base type
+	p = &x                   //p, of type *int, points to x
+	fmt.Println("*p = ", *p) //prints "1"
+	*p = 2                   //assign/updates the value of x
+	fmt.Println("x = ", x)   //prints the new value that is "2"
 }
--- src/08maps.go
+++ src/08maps.go
@@ -17,15 +17,13 @@ func main() {
 
 	//Map elements are accessed through the usual subscript notation:
 	ages["alice"] = 32
-	fmt.Println(ages["alice"]) // "32"
+	fmt.Println("alice = ", ages["alice"]) // "32"
 
 	//removed with the built-in function delete:
 	delete(ages, "alice") // remove element ages["alice"]
 
 	/*
-		All of these operations are safe even if the element isn’t in the map; a map lookup using a key
-		that isn’t present returns the zero value for its type, so, for instance, the following works even
-		when "bob" is not yet a key in the map because the value of ages["bob"] will be 0.
+		All of these operations are safe even if the element isn’t in the map
 	*/
 	ages["bob"] = ages["bob"] + 1
 	/*
@@ -34,8 +32,8 @@ func main() {
 		or
 		ages["bob"]++
 	*/
-	fmt.Println(ages["bob"])
-	/* But a map element is not a variable, and we cannot take its address:
+	fmt.Println("bob = ", ages["bob"])
+	/* a map element is not a variable, and we cannot take its address:
 
 	_=&ages["bob"] // compile error: cannot take address of map element
 
